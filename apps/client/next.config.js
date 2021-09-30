21aa1442c7420ed5de3e53504cef366ccc787ac7
Nickie Buckner <nickie.buckner@onecowstanding.com>
2021-09-30 13:11:17 -0400

    Fixed the issue by using the copy-webpack-plugin and moving the schema.prisma file and query engine binary to the appropriate chunks folder.

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,9 +1,22 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   reactStrictMode: true,
   webpack: (config, {isServer}) => {
     if (isServer) {
       config.externals.push('_http_common');
     }
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [{
+          from: '../../node_modules/.prisma/client/schema.prisma',
+          to: './'
+        }, {
+          from: '../../node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node',
+          to: './'
+        }]
+      })
+    )
     return config;
   }
 };
